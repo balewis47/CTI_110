344b644f3253e0d95c61ef7796cb8054183c6ce3
balewis47 <160041449+balewis47@users.noreply.github.com>
2024-02-14 10:36:40 -0500

    first python submission

---
+++ P1LAB1_LewisBetty.py
@@ -0,0 +1,11 @@
+#Betty Lewis
+#2/12/2024
+#P1LAB1
+#Using input and output
+
+#Creat a variable to hold the first name
+first_name = input("Enter you first name: ")
+
+#Print first_name with a greeting
+print("Hey", first_name) 
+print("Welcome to CTI110")
