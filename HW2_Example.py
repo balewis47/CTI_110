73a08c704f08a4207952a1470ccc2e9c084d90b4
balewis47 <160041449+balewis47@users.noreply.github.com>
2024-02-19 16:32:12 +0000

    first upload

---
+++ HW2_Example.py
@@ -0,0 +1,20 @@
+#Name
+#Date
+#Input and Output
+
+'''
+Pseudocode:
+Get buddget (int)
+Get destination (str)
+Get Gas (int)
+Get hotel (int)
+Get food (int)
+Add expenses 
+budget - expenses 
+Display results - use example
+'''
+
+pring ("This program caluclates and displays travel")
+# Get budget from user
+budget = int(input("Enter budget: "))
+dest = input("Enter your destination")
