f826e543350467962633007cbb228abfe0242f05
balewis47 <160041449+balewis47@users.noreply.github.com>
2024-02-19 23:20:54 -0500

    Add files via upload

---
+++ P1HW1_LewisBetty.py
@@ -0,0 +1,19 @@
+#Betty Lewis
+#2/19/24
+#P1HW1
+#Collectsing info from user
+
+
+user_num = int(input("Enter integer:\n"))
+
+print("You entered: ", user_num)
+
+print(user_num, "squared is", user_num * user_num)
+ 
+print("And", user_num, "cubed is", user_num * user_num*user_num, "!!")
+
+user_num2 = int(input("Enter another integer:\n"))
+
+print(user_num, "+", user_num2, "is", user_num + user_num2)
+
+print(user_num, "*", user_num2, "is", user_num * user_num2)
